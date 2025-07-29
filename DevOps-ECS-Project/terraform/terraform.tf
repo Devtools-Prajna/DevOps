3b492cdf0ff765d0354816a0b6c1cd83e29affaa
Vinod-M-git <vinodm@devtools.in>
2025-07-29 16:41:49 +0530

    update terraform.tf file

--- DevOps-ECS-Project/terraform/terraform.tf
+++ DevOps-ECS-Project/terraform/terraform.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "DevOps29072025"
-    key            = "/var/lib/jenkins/workspace/DevOps-ECS-Project/DevOps-ECS-Project/terraform/terraform.tfstate"
+    key            = "terraform/terraform.tfstate"
     region         = "us-west-1"
   }
 }
