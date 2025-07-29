62c295fd64f269e8bfdbda24b384608fa4f19161
Vinod-M-git <vinodm@devtools.in>
2025-07-29 16:45:55 +0530

    update bucket name

--- DevOps-ECS-Project/terraform/terraform.tf
+++ DevOps-ECS-Project/terraform/terraform.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "DevOps29072025"
+    bucket         = "devops29072025"
     key            = "terraform/terraform.tfstate"
     region         = "us-east-1"
   }
